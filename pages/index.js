620c91d44072fb23a4c21ed5fadd437852d1904d
Stephen Foks <stephen.foks@9thco.com>
2022-12-01 15:08:24 -0500

    feature: grid layout for cards

--- pages/index.js
+++ pages/index.js
@@ -21,8 +21,13 @@ export default function Home() {
       <main className={styles.main}>
         <Banner buttonText="View stores nearby" handleOnClick={handleOnBannerButtonClick} />
         <Image src="/static/hero-image.png" width={700} height={400} className={styles.heroImage}/>
-        
-        <Card name='DarkHorse Coffee' imgUrl='/static/hero-image.png' href="/coffee-store/darkhorse-coffee"/>
+
+        <div className={styles.cardLayout}>
+          <Card name='DarkHorse Coffee' imgUrl='/static/hero-image.png' href="/coffee-store/darkhorse-coffee" className={styles.card}/>
+          <Card name='DarkHorse Coffee' imgUrl='/static/hero-image.png' href="/coffee-store/darkhorse-coffee" className={styles.card}/>
+          <Card name='DarkHorse Coffee' imgUrl='/static/hero-image.png' href="/coffee-store/darkhorse-coffee" className={styles.card}/>
+          <Card name='DarkHorse Coffee' imgUrl='/static/hero-image.png' href="/coffee-store/darkhorse-coffee" className={styles.card}/>
+        </div>
       </main>
 
     </div>
