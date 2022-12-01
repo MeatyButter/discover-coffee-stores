3adc1051448bba3e3ffc5c77dab63ba06321fc50
Stephen Foks <stephen.foks@9thco.com>
2022-12-01 11:42:00 -0500

    quiz: update for solution

--- pages/[id].js
+++ pages/[id].js
@@ -1,8 +1,17 @@
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 const Dynamic = () => {
     const router = useRouter();
-    return <h1>{router.query.id}</h1>;
+    const query = router.query.id;
+    return (
+        <>
+            <Head>
+                <title>dynamic route: {query}</title>
+            </Head>
+            <h1>This is a dynamic route: {query}</h1>
+        </>
+    );
 }
 
 export default Dynamic;
