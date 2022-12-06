1d02a67b7d0eaece3dc7843ca390cf55a2e60aa9
Stephen Foks <stephen.foks@9thco.com>
2022-12-06 15:17:54 -0500

    feature: connect to Unsplash

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -16,27 +16,27 @@ export async function getStaticProps(staticProps) {
   return {
     props: {
       coffeeStore: coffeeStores.find((coffeeStore) => {
-        return coffeeStore.fsq_id.toString() === params.id; //dynamic id
+        return coffeeStore.id.toString() === params.id; //dynamic id
       }),
     },
   };
 }
 
 export async function getStaticPaths() {
-    const coffeeStores = await fetchCoffeeStores();
+  const coffeeStores = await fetchCoffeeStores();
 
-    const paths = coffeeStores.map((coffeeStore) => {
-        return {
-            params: {
-                id: coffeeStore.fsq_id.toString(),
-            },
-        };
-    });
-
+  const paths = coffeeStores.map((coffeeStore) => {
     return {
-        paths,
-        fallback: true,
+      params: {
+        id: coffeeStore.id.toString(),
+      },
     };
+  });
+
+  return {
+    paths,
+    fallback: true,
+  };
 }
 
 const CoffeeStore = (props) => {
@@ -46,7 +46,7 @@ const CoffeeStore = (props) => {
     return <div>Loading...</div>;
   }
 
-  const { location, name, imgUrl } = props.coffeeStore;
+  const { name, address, neighbourhood, imgUrl } = props.coffeeStore;
 
   const handleUpvoteButton = () => {};
 
@@ -58,13 +58,16 @@ const CoffeeStore = (props) => {
       <div className={styles.container}>
         <div className={styles.col1}>
           <div className={styles.backToHomeLink}>
-            <Link href="/">Back to home</Link>
+            <Link href="/">&larr; Back to home</Link>
           </div>
           <div className={styles.nameWrapper}>
             <h1 className={styles.name}>{name}</h1>
           </div>
           <Image
-            src={imgUrl || "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"}
+            src={
+              imgUrl ||
+              "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+            }
             width={600}
             height={360}
             className={styles.storeImg}
@@ -73,14 +76,18 @@ const CoffeeStore = (props) => {
         </div>
 
         <div className={cls("glass", styles.col2)}>
-          <div className={styles.iconWrapper}>
-            <Image src="/static/icons/places.svg" width="24" height="24" />
-            <p className={styles.text}>{location.address}</p>
-          </div>
-          <div className={styles.iconWrapper}>
-            <Image src="/static/icons/nearMe.svg" width="24" height="24" />
-            <p className={styles.text}>{location.neighborhood[0]}</p>
-          </div>
+          {address && (
+            <div className={styles.iconWrapper}>
+              <Image src="/static/icons/places.svg" width="24" height="24" />
+              <p className={styles.text}>{address}</p>
+            </div>
+          )}
+          {neighbourhood && (
+            <div className={styles.iconWrapper}>
+              <Image src="/static/icons/nearMe.svg" width="24" height="24" />
+              <p className={styles.text}>{neighbourhood}</p>
+            </div>
+          )}
           <div className={styles.iconWrapper}>
             <Image src="/static/icons/star.svg" width="24" height="24" />
             <p className={styles.text}>1</p>
