4523aa0742744ce51f3e05500cf801d24a057e82
Stephen Foks <stephen.foks@9thco.com>
2022-12-07 14:42:17 -0500

    feature: apply context to dynamic page

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -7,6 +7,9 @@ import cls from "classnames";
 
 import styles from "../../styles/coffee-store.module.css";
 import { fetchCoffeeStores } from "../../lib/coffee-stores";
+import { useContext, useEffect, useState } from "react";
+import { StoreContext } from "../_app";
+import { isEmpty } from "../../utils";
 
 export async function getStaticProps(staticProps) {
   const params = staticProps.params;
@@ -40,14 +43,35 @@ export async function getStaticPaths() {
   };
 }
 
-const CoffeeStore = (props) => {
+const CoffeeStore = (initialProps) => {
   const router = useRouter();
 
   if (router.isFallback) {
     return <div>Loading...</div>;
   }
 
-  const { name, address, neighbourhood, imgUrl } = props.coffeeStore;
+  const id = router.query.id;
+
+  const [coffeeStore, setCoffeeStore] = useState(initialProps.coffeeStore)
+
+  const {
+    state: {
+      coffeeStores
+    }
+  } = useContext(StoreContext);
+
+  useEffect(() => {
+    if(isEmpty(initialProps.coffeeStore)) {
+      if (coffeeStores.length > 0) {
+        const findCoffeeStoreById = coffeeStores.find((coffeeStore) => {
+          return coffeeStore.id.toString() === id; //dynamic id
+        })
+        setCoffeeStore(findCoffeeStoreById);
+      }
+    }
+  }, [id]);
+
+  const { name, address, neighbourhood, imgUrl } = coffeeStore;
 
   const handleUpvoteButton = () => {};
 
