26b367dc2de9a674386611435705d1c21b417274
Stephen Foks <stephen.foks@9thco.com>
2022-12-07 14:00:00 -0500

    feature: introduction to reducer applied to context

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -12,12 +12,13 @@ export async function getStaticProps(staticProps) {
   const params = staticProps.params;
 
   const coffeeStores = await fetchCoffeeStores();
+  const findCoffeeStoreById = coffeeStores.find((coffeeStore) => {
+    return coffeeStore.id.toString() === params.id; //dynamic id
+  })
 
   return {
     props: {
-      coffeeStore: coffeeStores.find((coffeeStore) => {
-        return coffeeStore.id.toString() === params.id; //dynamic id
-      }),
+      coffeeStore: findCoffeeStoreById? findCoffeeStoreById: {},
     },
   };
 }
